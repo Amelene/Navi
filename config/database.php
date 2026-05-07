9cc743f940d266e50434b3cc08f9b9f21082495c
Amelene <amelene10@gmail.com>
2026-05-07 13:23:45 +0800

    database.php

--- config/database.php
+++ config/database.php
@@ -97,7 +97,7 @@ $defaultLive = [
     'DB_USER' => 'navizaft_navi',
     'DB_PASS' => 'Admin@navi',
     'DB_CHARSET' => 'utf8mb4',
-    'DB_DEBUG' => true,
+    'DB_DEBUG' => false,
 ];
 
 $useLocalDefaults = isLocalEnvironment();
