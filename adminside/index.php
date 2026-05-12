1a53d6786f8265d721943e006c6a56135ec61630
Amelene <amelene10@gmail.com>
2026-05-12 15:36:21 +0800

    index.php

--- adminside/index.php
+++ adminside/index.php
@@ -686,7 +686,7 @@ try {
 
 		const overviewAllValues = overviewChartDatasets.flatMap(ds => Array.isArray(ds.data) ? ds.data : []);
 		const overviewMaxValue = overviewAllValues.length ? Math.max(...overviewAllValues, 0) : 0;
-		const overviewTickValues = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+		const overviewTickValues = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100].filter(v => v < overviewMaxValue);
 
 		new Chart(ovCtx, {
 			type: 'bar',
